3b778252358378b1465039b8523fbcb60d551e78
jmpau <jmpau@JMP_WORK>
2026-08-28 07:30:58 -0600

    Add project files.

---
+++ Say My Name/Program.cs
@@ -0,0 +1,16 @@
+﻿//Jonathan_Paul
+//RCET_2265
+//Fall_2026
+//
+
+
+namespace Say_My_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, What is your name");
+        }
+    }
+}
